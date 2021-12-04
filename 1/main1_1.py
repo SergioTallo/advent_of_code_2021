0e6f581e935ed415afccd4c9410623bbee389127
Sergio Tallo Torres <ic19b047@technikum-wien.at>
2021-12-04 14:16:06 +0100

    Advent of Code 2021 comments days 1

--- 1/main1_1.py
+++ 1/main1_1.py
@@ -1,10 +1,15 @@
+# Initialize counter to store the results
 counter = 0
 
+# Open file with the inputs and store every line (as an int) in an element of a list
 with open('input.txt') as f:
     values = [int(i) for i in f.readlines()]
 
+# Go through every element of the list (starting with the second) and compare every element with the previous one
 for i in range(1, len(values)):
+    # If the element is bigger than the previous one increase the counter by one
     if values[i] > values[i-1]:
         counter += 1
 
+# Show the results
 print(counter)
--- 1/main1_2.py
+++ 1/main1_2.py
@@ -1,14 +1,22 @@
+# Initialize counter to store the results
 counter = 0
 
+# Open file with the inputs and store every line (as an int) in an element of a list
 with open('input.txt') as f:
     values = [int(i) for i in f.readlines()]
 
+# Initialize element with the sum of the first three values
 prev_value = (values[0] + values[1] + values[2])
 
+# Go through every element (starting with the second one and till the element (last -2))
 for i in range(1, len(values) - 2):
+    # Sum the values of the element and the next two ones in a new value
     value = (values[i] + values[i+1] + values[i+2])
+    # Compare with the value previously stored
     if value > prev_value:
         counter += 1
+    # The new value becomes the old value to be compared with the next one
     prev_value = value
 
+# Print the results
 print(counter)
